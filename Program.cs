d3957649f9c7574544ba7abe0376021902ff7d62
yasminluisa <126296748+yasminluisa@users.noreply.github.com>
2023-07-01 22:03:54 -0300

    alcool ou gasolina

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("--- Etanol ou Gasolina? ---\n");
+
+Console.Write("Digite o preço do etanol (R$).....: ");
+decimal etanol = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite o preço da gasolina (R$)...: ");
+decimal gasolina = Convert.ToDecimal(Console.ReadLine());
+
+string recomendacao;
+
+if (AbastecerComGasolina(etanol, gasolina))
+{
+    recomendacao = "Gasolina";
+}
+else
+{
+    recomendacao = "Etanol";
+}
+
+double razao = RazaoEtanolGasolina(etanol, gasolina) * 100;
+
+Console.WriteLine($"\nO preço do etanol corresponde a {razao:N1}% do preço da gasolina.");
+Console.WriteLine($"\nRecomendação: Abasteça com {recomendacao.ToUpper()}.");
+
+double RazaoEtanolGasolina(decimal precoEtanol, decimal precoGasolina)
+{
+    return Convert.ToDouble(precoEtanol / precoGasolina);
+}
+
+bool AbastecerComGasolina(decimal precoEtanol, decimal precoGasolina)
+{
+    const double MinRazaoUsoGasolina = 0.73;
+    return RazaoEtanolGasolina(precoEtanol, precoGasolina) >= MinRazaoUsoGasolina;
+}
